5ce51d177d9005df9b138465a10670832eb6e832
sunyunfeng <67753460@qq.com>
2022-05-11 22:12:55 +0800

    add day10 - day14

---
+++ src/main/java/cloud/ry/day18num21/num21MergeTwoSortedLists_insert.java
@@ -0,0 +1,101 @@
+package cloud.ry.day18num21;
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+// Related Topics 递归 链表 👍 2385 👎 0
+
+import cloud.ry.common.ListNode;
+
+public class num21MergeTwoSortedLists_insert {
+    public static void main(String[] args) {
+        Solution solution = new num21MergeTwoSortedLists_insert().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode head = new ListNode();
+            head.next = list1;
+            ListNode a = head;
+            ListNode b = head.next;
+            ListNode c = list2;
+            if (c == null) {
+                return b;
+            }
+            if (b == null) {
+                return c;
+            }
+            ListNode d = c.next;
+            while (true) {
+                if (c.val < b.val) {
+                    a.next = c;
+                    c.next = b;
+                    // c插入a b 中间后, 只需要移动c d
+                    if (d == null) {
+                        break;
+                    }
+                    c = d;
+                    d = d.next;
+                    b = a.next;
+                } else {
+                    if (b.next == null) {
+                        b.next = c;
+                        break;
+                    }
+                    a = b;
+                    b = b.next;
+                }
+            }
+
+
+            return head.next;
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
